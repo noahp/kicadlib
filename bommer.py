c33da0a2b1fb1c31d2700651d7bb4efb959fb455
np <np@np.np>
2015-11-10 10:02:27 -0500

    fix bommer, add 2 & 5-pin generic headers

--- bommer.py
+++ bommer.py
@@ -7,8 +7,12 @@ outfile = sys.argv[2]
 xmldata = []
 tree = ET.parse(infile)
 for c in tree.findall('./components//comp'):
-    xmldata.append([c.find('./value').text, c.attrib['ref'], c.find('./footprint').text,])
+    result = [c.find('./value').text.encode('ascii', 'ignore'),
+              c.attrib['ref'],
+              (c.find('./footprint').text.encode('ascii', 'ignore')  if c.find('./footprint') is not None else 'None'),]
+    xmldata.append(result)
 
+        
 with open(outfile, 'wb') as f:
     csvwriter = csv.writer(f)
     csvwriter.writerow(['Value', 'Ref', 'Footprint'])
